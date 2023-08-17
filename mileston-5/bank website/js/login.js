6c26282a3016a5bd085a56f908ab277f79e008cb
abidalili <abidalili1995@gmail.com>
2023-08-17 23:14:55 +0300

    deposit page on progress

--- mileston-5/bank website/js/login.js
+++ mileston-5/bank website/js/login.js
@@ -13,8 +13,8 @@ document.getElementById("btn-submit").addEventListener("click", function () {
   //warning: do not verify email password on the client side
   //step-4: verify email and password and check whether valid user or not
   if (email === "abidalili@yahoo.com" && password === "Abidalili96") {
-    console.log("valid user");
+    window.location.href = "bank.html";
   } else {
-    console.log("invalid user");
+    alert("invalid user");
   }
 });
