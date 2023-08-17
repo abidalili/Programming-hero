ab05a213d26605e817e64af0c7f62a6ae1f73e3b
abidalili <abidalili1995@gmail.com>
2023-08-17 20:34:37 +0300

    home page done

--- mileston-5/bank website/js/login.js
+++ mileston-5/bank website/js/login.js
@@ -1,5 +1,20 @@
-const btn = document
-  .getElementById("btn-submit")
-  .addEventListener("click", function () {
-    console.log("submit btn clicked");
-  });
+//Step-1: add click event handler with the submit button
+document.getElementById("btn-submit").addEventListener("click", function () {
+  //step-2: get the email address inside the email input field
+  //always remember to use .value to get text from input field
+  const emailField = document.getElementById("user-email");
+  const email = emailField.value;
+  //   step-3: get password
+  //33.a: set id on the html element
+  //3.b:get the element
+  //3.c: get the value from the element
+  const passwordField = document.getElementById("user-password");
+  const password = passwordField.value;
+  //warning: do not verify email password on the client side
+  //step-4: verify email and password and check whether valid user or not
+  if (email === "abidalili@yahoo.com" && password === "Abidalili96") {
+    console.log("valid user");
+  } else {
+    console.log("invalid user");
+  }
+});
